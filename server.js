d145c8e36ea9ecef0401bafc87809d6dda958e41
Ludwring Liccien <ludwring.liccien@gmail.com>
2016-07-03 01:08:57 -0400

    Definición de vistas con Pug

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+// Requerir express
 var express = require('express');
 
+// Inicializar express en la variable app
 var app = express();
 
+// Setear pug como motor de plantillas html
+app.set('view engine', 'pug');
+
+// Servir o dar acceso a la carpeta public de forma virtual no es necesario colocar public en las rutas html
+app.use(express.static('public'));
+
+// Crear ruta / y responder a ella
 app.get('/', function (req, res) {
-	res.send('Hola Mundo!');
+	// res.send('Hola Mundo!');
+	res.render('index')
 })
 
+// Poner al server web a escuchar en el puerto 3000
 app.listen(3000, function (err) {
 	if (err) return console.log('Hubo un error'), process.exit(1)
 	
