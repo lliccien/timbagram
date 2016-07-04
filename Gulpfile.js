27be70271ff822dbaa8be39eb04247da7b2de083
Ludwring Liccien <ludwring.liccien@gmail.com>
2016-07-03 23:19:15 -0400

    Utilizando watchify y npm scripts para automatizar la ejecucion del server y las tareas de gulp en el comando npm start

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify =require('watchify');
 
 gulp.task('styles', function () {
 	gulp
@@ -19,13 +20,33 @@ gulp.task('assets', function () {
 		.pipe(gulp.dest('public/'))
 })
 
-gulp.task('scripts', function() {
-    browserify('./src/index.js')
-    	.transform(babel)
-    	.bundle()
-    	.pipe(source('index.js'))
-    	.pipe(rename('app.js'))
-    	.pipe(gulp.dest('public/js/'))
+function compile(watch){
+	var bundle = watchify(browserify('./src/index.js'));
+
+	function rebundle() {
+		bundle
+			.transform(babel)
+	    	.bundle()
+	    	.pipe(source('index.js'))
+	    	.pipe(rename('app.js'))
+	    	.pipe(gulp.dest('public/js/'))
+	}
+
+	if (watch) {
+		bundle.on('update', function(){
+			console.log('--> building...');
+			rebundle();
+		})
+	}
+	rebundle();
+}
+
+gulp.task('build', function() {
+    return compile();
 })
 
-gulp.task('default', ['styles', 'assets', 'scripts'])
+gulp.task('watch', function() {
+    return compile(true);
+})
+
+gulp.task('default', ['styles', 'assets', 'build'])
