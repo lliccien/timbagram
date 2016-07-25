f07c8613d02cb06f4fc44249dd8ad9dc72b63c13
Ludwring Liccien <ludwring.liccien@gmail.com>
2016-07-24 21:10:04 -0400

    Subiendo fotos a nuestro servidor

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 // Requerir express
 var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+
+var upload = multer({ storage: storage }).single('picture');
 
 // Inicializar express en la variable app
 var app = express();
@@ -66,6 +79,16 @@ app.get('/api/pictures', function (req, res) {
 	setTimeout(function () {
 		res.send(pictures);
 	}, 2000)
+
+});
+
+app.post('/api/pictures', function(req, res){
+	upload(req, res, function (err) {
+		if (err) {
+			return res.send(500, "Error uploading file");
+		}
+		res.send('File uploaded');
+	})
 
 })
 
